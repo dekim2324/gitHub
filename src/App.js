39188217af9659ae0194df01aec08af17deb60b5
Dennis Kim <dekim2324@gmail.com>
2019-12-25 02:01:10 -0600

    async

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './component/layout/Navbar';
-import Users from './component/users/Users'
+import Users from './component/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false })
+    
+  
+  }
 
   render() {
-
     return (
       <div className='App'>
         <Navbar />

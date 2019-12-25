817b83d877ed702ccb94a65cc27aaf3576af9559
Dennis Kim <dekim2324@gmail.com>
2019-12-25 02:22:03 -0600

    before restart

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     )
